c5e2adace88ff5adb71c0fb94d3de73e553357dc
Luke Walker <luke.walker@gmail.com>
2022-07-27 08:34:39 -0300

    Changes to JS after linting and prettifying.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import './styles.css';
+import './styles.css'
 
-var template = require("./js/studentinfo.hbs");
+const template = require('./js/studentinfo.hbs')
 
 async function populate() {
+  const requestURL = './assets/data.json?dothisfor=' + reportconfig.dothisfor
+  const request = new Request(requestURL)
 
-  const requestURL = './assets/data.json?dothisfor=' + reportconfig.dothisfor;
-  const request = new Request(requestURL);
+  const response = await fetch(request)
+  const data = await response.json()
 
-  const response = await fetch(request);
-  const data = await response.json();
-
-  let container = document.getElementById("output");
-  container.innerHTML = template(data);
+  const container = document.getElementById('output')
+  container.innerHTML = template(data)
 }
- 
-populate();
+
+populate()
