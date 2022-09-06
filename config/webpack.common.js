9ff9811f3a38ce21bdd16b2fc3f8a9371be30890
Luke Walker <luke.walker@gmail.com>
2022-09-06 20:57:57 -0300

    Working multi-page build

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,10 +1,8 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const paths = require('./paths')
 
-const pages = ["psb_int3","psb_int4"];
+const pages = ["psb_int","psb_int4"];
 
 module.exports = {
   // Where webpack looks to start building the bundle
@@ -28,36 +26,18 @@ module.exports = {
   },
 
   // Customize the webpack build process
-  plugins: [].concat(
+
+  plugins: 
+  [].concat(
     pages.map(
       (page) =>
-        new HtmlWebpackPlguin({
+        new HtmlWebpackPlugin({
           inject: true,
-          template: `./${page}.html`,
+          template: `./src/${page}.html`,
           filename: `${page}.html`,
           chunks: [page]
         })
     ),
-  
-  [
-    // Removes/cleans build folders and unused assets when rebuilding
-    new CleanWebpackPlugin(),
-
-    // Copies files from target to destination folder
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: paths.public,
-          to: 'assets',
-          globOptions: {
-            ignore: ['*.DS_Store'],
-          },
-          noErrorOnMissing: true,
-        },
-      ],
-
-    }),
-  ],
 
   ),
   // Determine how modules within the project are treated'
