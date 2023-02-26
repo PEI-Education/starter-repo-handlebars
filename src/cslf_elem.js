a1ec71754bcee14f16cebac16c1bbea714f2b2d3
Luke Walker <lawalker@gov.pe.ca>
2023-02-26 02:40:49 -0400

    Initial CSLF-elem rebuild for testing

--- src/cslf_elem.js
+++ src/cslf_elem.js
@@ -2,3 +2,40 @@ import './styles/cslf_elem.css';
 import './styles/spinner.css';
 
 const template = require('./js/cslf_elem.hbs')
+
+const dataSource = `./assets/cslf_elem.json?dothisfor=${reportconfig.dothisfor}&coteachers=${reportconfig.coteachers}&terms=${reportconfig.terms}&yearid=${reportconfig.yearid}`
+
+const fadeOutEffect = () => {
+   let fadeTarget = document.getElementById("overlay");
+   let fadeEffect = setInterval(function () {
+      if (!fadeTarget.style.opacity) {
+         fadeTarget.style.opacity = .8;
+      }
+      if (fadeTarget.style.opacity > 0) {
+         fadeTarget.style.opacity -= 0.1;
+      } else {
+         clearInterval(fadeEffect);
+      }
+   }, 200);
+}
+
+function process(students) {
+
+   const outputData = { reportconfig: reportconfig, students: students }
+   const container = document.getElementById('output')
+   container.innerHTML = template(outputData)
+   const overlay = document.getElementById('overlay') 
+   fadeOutEffect()
+   overlay.remove()
+
+}
+
+const populate = async (url) => {
+
+   const response = await fetch(url)
+   const results = await response.json()
+   process(results)
+
+}
+
+populate(dataSource)
